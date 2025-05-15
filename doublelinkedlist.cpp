a399937e81c422e9f368fd3baf8b515c8e728f0b
Nvann07 <nrizwardi@gmail.com>
2025-05-15 18:33:43 +0700

    Membuat class noide

---
+++ doublelinkedlist.cpp
@@ -0,0 +1,18 @@
+// 0069_DoubLeLinkedList
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+  public:
+   int noMhs;
+   Node *next;
+   Node *prev;
+};
+
+class Doublelinkedlist
+{
+
+}
